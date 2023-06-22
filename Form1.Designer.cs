7a74566953329d2ae0101a431bf6dbca120eb2f3
Faby121753 <m.f.vasconez@hotmail.com>
2023-06-22 01:46:31 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace FM
+{
+		partial class Form1
+		{
+			private System.ComponentModel.IContainer components = null;
+			private System.Windows.Forms.Button btnGenerarAnimal;
+			private System.Windows.Forms.PictureBox pictureBoxAnimal;
+			private System.Windows.Forms.Label lblAnimal;
+
+			protected override void Dispose(bool disposing)
+			{
+				if (disposing && (components != null))
+				{
+					components.Dispose();
+				}
+				base.Dispose(disposing);
+			}
+
+			private void InitializeComponent()
+			{
+			this.btnGenerarAnimal = new System.Windows.Forms.Button();
+			this.lblAnimal = new System.Windows.Forms.Label();
+			this.pictureBoxAnimal = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxAnimal)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// btnGenerarAnimal
+			// 
+			this.btnGenerarAnimal.Location = new System.Drawing.Point(172, 376);
+			this.btnGenerarAnimal.Margin = new System.Windows.Forms.Padding(4);
+			this.btnGenerarAnimal.Name = "btnGenerarAnimal";
+			this.btnGenerarAnimal.Size = new System.Drawing.Size(160, 49);
+			this.btnGenerarAnimal.TabIndex = 0;
+			this.btnGenerarAnimal.Text = "Generar Animal";
+			this.btnGenerarAnimal.UseVisualStyleBackColor = true;
+			this.btnGenerarAnimal.Click += new System.EventHandler(this.btnGenerarAnimal_Click);
+			// 
+			// lblAnimal
+			// 
+			this.lblAnimal.AutoSize = true;
+			this.lblAnimal.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblAnimal.Location = new System.Drawing.Point(16, 222);
+			this.lblAnimal.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.lblAnimal.Name = "lblAnimal";
+			this.lblAnimal.Size = new System.Drawing.Size(0, 29);
+			this.lblAnimal.TabIndex = 1;
+			// 
+			// pictureBoxAnimal
+			// 
+			this.pictureBoxAnimal.Location = new System.Drawing.Point(144, 79);
+			this.pictureBoxAnimal.Margin = new System.Windows.Forms.Padding(4);
+			this.pictureBoxAnimal.Name = "pictureBoxAnimal";
+			this.pictureBoxAnimal.Size = new System.Drawing.Size(221, 244);
+			this.pictureBoxAnimal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBoxAnimal.TabIndex = 2;
+			this.pictureBoxAnimal.TabStop = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(512, 471);
+			this.Controls.Add(this.pictureBoxAnimal);
+			this.Controls.Add(this.lblAnimal);
+			this.Controls.Add(this.btnGenerarAnimal);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Margin = new System.Windows.Forms.Padding(4);
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxAnimal)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+			}
+		}
+	
+
+}
+
---
+++ Form1.cs
@@ -0,0 +1,78 @@
+﻿using FactoryMeth;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FM
+{
+	public partial class Form1 : Form
+	{
+		private List<Animal> listaAnimales;
+		private Random random;
+
+		public Form1()
+		{
+			InitializeComponent();
+			InitializeAnimales();
+			random = new Random();
+		}
+
+		private void InitializeAnimales()
+		{
+			listaAnimales = new List<Animal>
+			{
+				new Vaca(),
+				new Pajaro(),
+				new Serpiente()
+			};
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			UpdateAnimalImage();
+		}
+
+		private void btnGenerarAnimal_Click(object sender, EventArgs e)
+		{
+			UpdateAnimalImage();
+		}
+
+		private void UpdateAnimalImage()
+		{
+			Animal animal = listaAnimales[random.Next(listaAnimales.Count)];
+			string animalName = animal.Render();
+
+			lblAnimal.Text = animalName;
+
+			// Actualizar la imagen del PictureBox
+			Image image = GetAnimalImage(animalName);
+			pictureBoxAnimal.Image = image;
+		}
+
+		private Image GetAnimalImage(string animalName)
+		{
+			switch (animalName)
+			{
+				case "Vaca":
+					return Properties.Resources.Vaca;
+				case "Pajaro":
+					return Properties.Resources.Pajaro;
+				case "Serpiente":
+					return Properties.Resources.Serpiente;
+				default:
+					return null;
+			}
+		}
+
+		private void Form1_Load_1(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
